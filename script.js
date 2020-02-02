13b9fe38f1d874799d30b0202f73a110667951eb
marijedb <49620465+marijedb@users.noreply.github.com>
2020-02-02 02:24:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,19 +6,19 @@ var body = document.getElementById("body");
 function setColor(){
     body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 
-    css.textContent = body.style.background + ";";
+//     css.textContent = body.style.background + ";";
 }
-// function setColorText(){
-//     css.innerHTML = "linear-gradient(to right, " + color1.value + ", " + color2.value + ");";
-// }
+function setColorText(){
+    css.innerHTML = "linear-gradient(to right, " + color1.value + ", " + color2.value + ");";
+}
 
 color1.addEventListener("input", setColor);
-// color1.addEventListener("input", setColorText);
+color1.addEventListener("input", setColorText);
 
 color2.addEventListener("input", setColor);
-// color2.addEventListener("input", setColorText);
+color2.addEventListener("input", setColorText);
 
-// setColorText();
+setColorText();
 
 
-// All the commented out stuff is to change the h3 value into the #color value instead of the rgb() value. 
+// The commented out line is to use the rgb() color value instead of the # color value. 
